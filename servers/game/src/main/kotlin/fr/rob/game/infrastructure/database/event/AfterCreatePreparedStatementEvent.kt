90d2a83fa18b69de9b39e06ee5a0dbbcbd8f049b
Axel <axel.leduc@clickandboat.com>
2021-01-04 12:59:20 +0100

    Quickfix: Database event arguments visibility

--- servers/game/src/main/kotlin/fr/rob/game/infrastructure/database/event/AfterCreatePreparedStatementEvent.kt
+++ servers/game/src/main/kotlin/fr/rob/game/infrastructure/database/event/AfterCreatePreparedStatementEvent.kt
@@ -1,6 +1,6 @@
 package fr.rob.game.infrastructure.database.event
 
-class AfterCreatePreparedStatementEvent(private val sql: String, private val time: Long) : DatabaseEvent(sql, time) {
+class AfterCreatePreparedStatementEvent(sql: String, time: Long) : DatabaseEvent(sql, time) {
 
     override fun getName(): String = AFTER_CREATE_PREPARED_STATEMENT
 
--- servers/game/src/main/kotlin/fr/rob/game/infrastructure/database/event/AfterSQLReqExecutedEvent.kt
+++ servers/game/src/main/kotlin/fr/rob/game/infrastructure/database/event/AfterSQLReqExecutedEvent.kt
@@ -1,7 +1,7 @@
 package fr.rob.game.infrastructure.database.event
 
 
-class AfterSQLReqExecutedEvent (private val sql: String, private val time: Long) : DatabaseEvent(sql, time) {
+class AfterSQLReqExecutedEvent (sql: String, time: Long) : DatabaseEvent(sql, time) {
 
     override fun getName(): String = AFTER_SQL_REQ_EXECUTED_EVENT
 
