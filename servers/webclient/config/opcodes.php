cb428341616073184b3c48c74d3af8f2a632f24e
Axel <axel.leduc@clickandboat.com>
2021-02-01 11:59:37 +0100

    Refs #19: WIP Symfony Web client (scenarios)

--- servers/webclient/config/opcodes.php
+++ servers/webclient/config/opcodes.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
+define('CMSG_AUTHENTICATE_SESSION', 0x00);
+
 return static function (ContainerConfigurator $container) {
     $container->parameters()
         ->set('opcodes', [
-            'CMSG_AUTHENTICATION' => [
-                'id' => 0x01,
+            'CMSG_AUTHENTICATE_SESSION' => [
+                'id' => CMSG_AUTHENTICATE_SESSION,
                 'message' => Rob\Protos\Auth::class
             ]
         ]);
