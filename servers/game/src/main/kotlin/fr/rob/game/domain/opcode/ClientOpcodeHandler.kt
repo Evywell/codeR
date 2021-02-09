7412960be85b045c00fc546bb59eaa8ed31d2280
Axel <axel.leduc@clickandboat.com>
2021-02-09 15:51:17 +0100

    Refs #31: WIP Character stand opcode

--- servers/game/src/main/kotlin/fr/rob/game/domain/opcode/ClientOpcodeHandler.kt
+++ servers/game/src/main/kotlin/fr/rob/game/domain/opcode/ClientOpcodeHandler.kt
@@ -1,13 +1,15 @@
 package fr.rob.game.domain.opcode
 
-import fr.rob.core.BaseApplication
 import fr.rob.core.log.LoggerInterface
 import fr.rob.core.opcode.OpcodeHandler
 import fr.rob.core.process.ProcessManager
+import fr.rob.game.domain.game.OpcodeClient
+import fr.rob.game.domain.game.character.stand.CharacterStandOpcode
 
-class ClientOpcodeHandler(private val processManager: ProcessManager, app: BaseApplication, logger: LoggerInterface) :
+class ClientOpcodeHandler(private val processManager: ProcessManager, logger: LoggerInterface) :
     OpcodeHandler(logger) {
 
     override fun initialize() {
+        registerOpcode(OpcodeClient.CHARACTER_STAND, CharacterStandOpcode(processManager))
     }
 }
