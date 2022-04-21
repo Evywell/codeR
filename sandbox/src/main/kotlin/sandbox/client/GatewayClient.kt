eaf8c1d16cb1dd81d21fc8e8f1efba7f030b16f4
Axel LEDUC <axel.leduc@clickandboat.com>
2022-04-21 12:07:18 +0200

    Refs #83: feat(Gateway communicate with other service) Removing useless client

--- sandbox/src/main/kotlin/sandbox/client/GatewayClient.kt
+++ sandbox/src/main/kotlin/sandbox/client/GatewayClient.kt
@@ -1,16 +1,16 @@
-package fr.rob.gateway.tmp
+package sandbox.client
 
 import fr.rob.core.network.v2.AbstractClient
 import fr.rob.core.network.v2.session.Session
 import fr.rob.core.network.v2.session.SessionSocketInterface
-import fr.rob.gateway.message.GatewayProto.Packet
+import fr.rob.gateway.message.GatewayProto
 
-class LeClient : AbstractClient<Packet>() {
+class GatewayClient : AbstractClient<GatewayProto.Packet>() {
     override fun onConnectionEstablished(session: Session) {
         this.session = session
     }
 
-    override fun onPacketReceived(packet: Packet) {
+    override fun onPacketReceived(packet: GatewayProto.Packet) {
         TODO("Not yet implemented")
     }
 
