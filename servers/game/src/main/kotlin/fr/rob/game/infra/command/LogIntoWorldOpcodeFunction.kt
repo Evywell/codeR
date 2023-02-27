5f45c86e7b5ca272d8e10985f3bc1a0afe9dc161
Leduc Axel <leduc.axel.pro@gmail.com>
2023-02-27 18:24:10 +0100

    Revert "Improvement: Fix proto message package destination import"
    This reverts commit d5d440893032491384f8ba11998feae1d1c73f22.

--- servers/game/src/main/kotlin/fr/rob/game/infra/command/LogIntoWorldOpcodeFunction.kt
+++ servers/game/src/main/kotlin/fr/rob/game/infra/command/LogIntoWorldOpcodeFunction.kt
@@ -2,11 +2,11 @@ package fr.rob.game.infra.command
 
 import com.google.protobuf.Message
 import fr.raven.proto.message.game.GameProto
-import fr.raven.proto.message.game.setup.LogIntoWorldProto
 import fr.rob.game.app.player.action.CreatePlayerIntoWorldCommand
 import fr.rob.game.app.player.action.CreatePlayerIntoWorldHandler
 import fr.rob.game.app.player.action.MovementCommand
 import fr.rob.game.app.player.action.MovementHandler
+import fr.rob.game.command.message.LogIntoWorldProto
 import fr.rob.game.domain.character.waitingroom.CharacterWaitingRoom
 import fr.rob.game.domain.entity.Movement
 import fr.rob.game.infra.opcode.GameNodeFunctionParameters
