14ea366e55b5d373610df47468e19dea427dcc22
Axel LEDUC <axel.leduc@clickandboat.com>
2023-10-06 18:27:14 +0200

    Simplify unit tests
    Improvement

--- servers/game/src/main/kotlin/fr/rob/game/domain/spell/effect/InstantDamageEffect.kt
+++ servers/game/src/main/kotlin/fr/rob/game/domain/spell/effect/InstantDamageEffect.kt
@@ -1,7 +1,6 @@
 package fr.rob.game.domain.spell.effect
 
 import fr.rob.game.domain.combat.DamageSource
-import fr.rob.game.domain.entity.Unit
 import fr.rob.game.domain.entity.WorldObject
 import fr.rob.game.domain.spell.Spell
 import fr.rob.game.domain.spell.target.SpellTargetParameter
@@ -13,9 +12,7 @@ class InstantDamageEffect(
 ) : EffectInterface {
     override fun cast(spellEffectSummary: SpellEffectSummary) {
         target.getPrimaryTarget().ifPresent { tar ->
-            val casterLevel = if (caster is Unit) caster.level else 1
-
-            spellEffectSummary.addDamageSourceForTarget(tar, DamageSource(caster, spellEffectInfo.value * casterLevel))
+            spellEffectSummary.addDamageSourceForTarget(tar, DamageSource(caster, spellEffectInfo.value))
         }
     }
 
