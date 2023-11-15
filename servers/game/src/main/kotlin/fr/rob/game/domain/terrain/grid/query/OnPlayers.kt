ced95d8c904bc6d4041f7afca4842b77c076325c
Axel LEDUC <axel.leduc@clickandboat.com>
2023-11-15 19:08:14 +0100

    Refs #112 - refactor the opcode/message system of game server

--- servers/game/src/main/kotlin/fr/rob/game/domain/terrain/grid/query/OnPlayers.kt
+++ servers/game/src/main/kotlin/fr/rob/game/domain/terrain/grid/query/OnPlayers.kt
@@ -3,7 +3,6 @@ package fr.rob.game.domain.terrain.grid.query
 import fr.rob.game.domain.entity.WorldObject
 import fr.rob.game.domain.entity.guid.ObjectGuid
 import fr.rob.game.domain.terrain.grid.Grid
-import fr.rob.game.domain.terrain.grid.query.GridQueryDataSetInterface
 
 class OnPlayers : GridQueryDataSetInterface {
     override fun getObjects(grid: Grid): List<WorldObject> = grid.getObjectsByType(ObjectGuid.GUID_TYPE.PLAYER)
