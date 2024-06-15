30b274653f385b6e4651867927e49a82eda332cc
Axel LEDUC <axel.leduc@clickandboat.com>
2024-06-15 18:35:20 +0200

    improvement(Damage calculation): Remove useless? field

--- servers/game/src/main/kotlin/fr/rob/game/domain/combat/DamageSource.kt
+++ servers/game/src/main/kotlin/fr/rob/game/domain/combat/DamageSource.kt
@@ -2,4 +2,4 @@ package fr.rob.game.domain.combat
 
 import fr.rob.game.domain.entity.WorldObject
 
-data class DamageSource(val source: WorldObject, val amount: Int, var isCritical: Boolean = false)
+data class DamageSource(val source: WorldObject, val amount: Int)
