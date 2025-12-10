5402667bc07c7fc1d3542bdc4d0ef929eb3043d7
Axel Leduc <axel.leduc@clickandboat.com>
2025-12-10 14:46:02 +0100

    Remove deprecated ProcessManager

--- servers/game/src/test/kotlin/fr/rob/game/test/unit/sandbox/process/CreateProcessWithArgumentsProcess.kt
+++
@@ -1,5 +0,0 @@
-package fr.rob.game.test.unit.sandbox.process
-
-import fr.rob.game.test.unit.domain.process.DependencyClass
-
-class CreateProcessWithArgumentsProcess(val dependency: DependencyClass)
