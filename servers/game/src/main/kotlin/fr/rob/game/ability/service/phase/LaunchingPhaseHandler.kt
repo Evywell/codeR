3870464c8958607f4fae292b568cb2b884756072
Axel Leduc <axel.leduc@clickandboat.com>
2026-06-23 19:30:45 +0200

    improve(ability system): improve launching classes to simplify

--- servers/game/src/main/kotlin/fr/rob/game/ability/service/phase/LaunchingPhaseHandler.kt
+++ servers/game/src/main/kotlin/fr/rob/game/ability/service/phase/LaunchingPhaseHandler.kt
@@ -1,22 +1,16 @@
 package fr.rob.game.ability.service.phase
 
 import fr.rob.game.ability.Ability
-import fr.rob.game.ability.launch.LaunchInfoInterface
 import fr.rob.game.ability.launch.UpdatableLaunchTypeInterface
 
-class LaunchingPhaseHandler(private val launchersInfo: List<LaunchInfoInterface>) : AbilityPhaseHandlerInterface {
+class LaunchingPhaseHandler : AbilityPhaseHandlerInterface {
     override fun canHandle(ability: Ability): Boolean =
         ability.state == Ability.AbilityState.LAUNCHING
 
     override fun handle(ability: Ability, deltaTime: Int) {
-        if (ability.launchType != null) {
-            handleAbilityLaunch(ability, deltaTime);
-
-            return
+        if (ability.launchType == null) {
+            ability.launchType = ability.info.launchType.createLauncher()
         }
-
-        val launcherInfo = launchersInfo.firstOrNull { it.support(ability) } ?: return
-        ability.launchType = launcherInfo.createAbilityLauncher(ability)
 
         handleAbilityLaunch(ability, deltaTime)
     }
