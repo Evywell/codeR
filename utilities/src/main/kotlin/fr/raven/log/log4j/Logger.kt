01c5e206578e96a7964c3bc5c9ee2ca1927ccf99
Axel LEDUC <leduc.axel.pro@gmail.com>
2026-09-20 21:18:58 +0200

    fix logger followed parameters bug

--- utilities/src/main/kotlin/fr/raven/log/log4j/Logger.kt
+++ utilities/src/main/kotlin/fr/raven/log/log4j/Logger.kt
@@ -7,19 +7,19 @@ class Logger(private val logger: Logger) : LoggerInterface {
 
     override fun debug(message: String, vararg parameters: Any) {
         if (logger.isDebugEnabled) {
-            logger.debug(message, parameters)
+            logger.debug(message, *parameters)
         }
     }
 
     override fun error(message: String, vararg parameters: Any) {
-        logger.error(message, parameters)
+        logger.error(message, *parameters)
     }
 
     override fun info(message: String, vararg parameters: Any) {
-        logger.info(message, parameters)
+        logger.info(message, *parameters)
     }
 
     override fun warning(message: String, vararg parameters: Any) {
-        logger.warn(message, parameters)
+        logger.warn(message, *parameters)
     }
 }
